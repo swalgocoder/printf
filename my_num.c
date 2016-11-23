102d3389313f9dc6f60ed16c7bad8dcc0627d351
swalgocoder <gary.d.gao@gmail.com>
2016-11-22 17:39:04 -0800

    Create my_num.c

---
+++ my_num.c
@@ -0,0 +1,58 @@
+#include "holberton.h"
+#include <stdarg.h>
+/**
+ *my_puts_str - str followed by a new line
+ *@str: string
+ *
+ */
+void my_puts_str(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
+/**
+ *my_percentage - % as an input
+ *@per: A %
+ *Return: the % char
+ */
+int my_percentage(int per)
+{
+	per = '%';
+	_putchar(per);
+	return (1);
+}
+
+
+int *itoa(int n, char *s, int b) 
+{
+	static char digits[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+	int i=0, sign;
+    
+	if ((sign = n) < 0)
+		n = -n;
+	do {
+		s[i++] = digits[n % b];
+	} while ((n /= b) > 0);
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	return (i);
+}
+
+/**
+ * my_number - prints numbers
+ * @n: number
+ * Return: number
+ */
+int my_number(int n)
+{
+char buffer[5];
+int b;
+b = 10;
+
+*itoa(n, buffer, 10);
+}
